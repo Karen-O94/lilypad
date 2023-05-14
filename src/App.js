5561adae157837ac89f5fb37116257fe79b218b1
azzywazzy <arianne@hey.com>
2023-05-14 19:55:09 +0100

    initial version which shows translation on the page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import Translate from './Translate';
+import Translate from './components/Translate';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +18,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
       <Translate />
     </div>
   );
