6bc80998aab76ac76c1614afe7537bc3a25edad5
Karen <karenyaa@live.co.uk>
2023-05-14 15:42:38 +0100

    amended eventlistener to format suitable for JSX

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,12 +4,18 @@ function Weather () {
     var latitude = "";
     var longitude = "";
 
-    const cityBtn = document.getElementById("city-btn"); 
+    //const cityBtn = document.getElementById("city-btn"); 
 
-    cityBtn.addEventListener("click", getCityLongLat);
+    function handleCitySubmit(event) {
+        event.preventDefault();
+        getCityLongLat();
+    };
 
-    async function getCityLongLat(event) {
-        event.preventDefault()
+    //cityBtn.addEventListener("click", getCityLongLat);
+
+    //Gets longitude and latitude co-ordinates from city input
+    async function getCityLongLat() {
+        //event.preventDefault()
         city = document.getElementById("main-search").value
         var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
         
@@ -24,13 +30,13 @@ function Weather () {
         <div class="container-fluid">
             <form className="d-flex" id="search-form" role="search">
             <input className="form-control me-2" id="main-search" type="search" placeholder="What city would you like to visit?" aria-label="Search"/>
-            <button className="btn btn-outline-success" id="city-btn" type="submit">Search</button>
+            <button className="btn btn-outline-success" onClick={handleCitySubmit} id="city-btn" type="submit">Search</button>
           </form>
 
 
 
             <div class="card">
-                <img  alt="Avatar" />
+                {/* <img  alt="Avatar" /> */}
                 <div className="container">
                     <h6 id="city"><b>City: </b></h6>
                     <p id="temp">Temp: </p>
