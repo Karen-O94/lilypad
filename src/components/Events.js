7911e18e1e058efa8977566e2fe5f34c8ef2a650
WittK95 <108486235+WittK95@users.noreply.github.com>
2023-05-21 20:46:23 +0100

    Integrated ticketMaster API call into events component for first event

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,24 +1,71 @@
-import Event1 from "../images/Event_1_placeholder.webp";
+// import Event1 from "../images/Event_1_placeholder.webp";
 import Event2 from "../images/Event_2_placeholder.webp";
 import Event3 from "../images/Event_3_placeholder.webp";
 import "./Events.css";
+import { useEffect, useState } from "react";
 
 function Events() {
+  let city = "Paris";
+
+  const [eventName, setEventName] = useState(
+    "no events listed, why not start your own?"
+  );
+  const [eventVenue, setEventVenue] = useState(" ");
+  const [eventDate, setEventDate] = useState(" ");
+  const [eventImage, setEventImage] = useState(" ");
+  const [eventLink, setEventLink] = useState(" ");
+
+  useEffect(() => {
+    async function TicketMasterAPI() {
+      const apiKey = "IGAYyVG0ECptEE5ZMHXObHV2YaNqAQxO";
+      const apiURL = "https://app.ticketmaster.com/discovery/v2/";
+
+      const response = await fetch(
+        apiURL + `events.json?city=${city}&apikey=${apiKey}`
+      );
+
+      var data = await response.json();
+
+      let eventIndex = Math.floor(Math.random() * data._embedded.events.length);
+
+      const date = new Date(
+        data._embedded.events[eventIndex].dates.start.localDate
+      );
+
+      const options = {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      };
+
+      const formattedDate = date.toLocaleString("en-UK", options);
+      // format event date into desired format
+
+      setEventName(data._embedded.events[eventIndex].name);
+      setEventVenue(data._embedded.events[eventIndex]._embedded.venues[0].name);
+      setEventDate(formattedDate);
+      setEventImage(data._embedded.events[eventIndex].images[1].url);
+      setEventLink(data._embedded.events[eventIndex].url);
+    }
+
+    TicketMasterAPI();
+  });
+
   return (
     <div className="events">
-      <div className="events-Header">
+      <div className="events-header">
         <h3>LOCAL EVENTS</h3>
       </div>
-      <div className="events-Body">
+      <div className="events-body">
         <div className="event1">
           <div className="event-image">
-            <a href="https://www.ticketmaster.co.uk/harry-styles-tickets/artist/5228999?int_cmp_name=Harry-Styles-2023&int_cmp_id=UK-Home-601&int_cmp_creative=Home-main-3&tm_link=tm_ccp_Home_main_Harry-Styles-2023#gallery">
-              <img src={Event1} alt="Event 1" />
+            <a href={eventLink}>
+              <img src={eventImage} alt="Event 1" />
             </a>
           </div>
-          <p>Harry Styles</p>
-          <p>Wembley Stadium</p>
-          <p>13th June 19:00</p>
+          <p>{eventName}</p>
+          <p>{eventVenue}</p>
+          <p>{eventDate}</p>
         </div>
         <div className="event2">
           <div className="events-image">
@@ -47,59 +94,6 @@ function Events() {
 
 export default Events;
 
-// let city = "Paris";
-
-// const apiKey = "IGAYyVG0ECptEE5ZMHXObHV2YaNqAQxO";
-// const apiUrl = "https://app.ticketmaster.com/discovery/v2/";
-
-// async function ticketMasterAPI() {
-//   const response = await fetch(
-//     apiUrl + `events.json?city=${city}&apikey=${apiKey}`
-//   );
-//   var data = await response.json();
-//   // API request
-
-//   let eventIndex = Math.floor(Math.random() * data._embedded.events.length);
-//   // generates a random number from the number of events
-
-//   document.querySelector(".name").innerHTML =
-//     data._embedded.events[eventIndex].name;
-//   // add event name to HTML
-
-//   document.querySelector(".venueName").innerHTML =
-//     data._embedded.events[eventIndex]._embedded.venues[0].name;
-//   // add venue name to HTML
-
-//   const date = new Date(
-//     data._embedded.events[eventIndex].dates.start.localDate
-//   );
-
-//   const options = {
-//     year: "numeric",
-//     month: "long",
-//     day: "numeric",
-//   };
-
-//   const formattedDate = date.toLocaleString("en-UK", options);
-//   // format event date into desired format
-
-//   document.querySelector(".dates").innerHTML = formattedDate;
-//   // add formatted event date to html
-
-//   document.querySelector(".times").innerHTML =
-//     data._embedded.events[eventIndex].dates.start.localTime;
-//   // add event time to html
-
-//   const img = document.getElementById("eventImage");
-//   img.src = data._embedded.events[eventIndex].images[1].url;
-//   // add event image to html
-
-//   const link = document.getElementById("eventLink");
-//   link.href = data._embedded.events[eventIndex].url;
-//   // add link to event image
-// }
-
-// ticketMasterAPI();
 // // To do:
 // // add 2nd and 3rd event
 // // Error handling so 'undefined' doesn't show up
