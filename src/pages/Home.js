33344e01413a7ed1d3dcf19d477a861d3389c9eb
Siham Dahir <98513419+sihamad@users.noreply.github.com>
2023-05-22 06:57:48 +0100

    Updated searchbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,16 @@
+import './Home.css'; 
+import TitleBanner from "../components/TitleBanner";
+import SearchBar from "../components/SearchBar";
 import React from 'react';
-import './Home.css';
-import TitleBanner from '../components/TitleBanner';
-import SearchBar from '../components/SearchBar';
-import LilypadLogo from '../images/LilypadLogo.png';
+
+
 
 const Home = () => {
-  return (
-    <div className="home-container">
-      <div className="title-banner">
-        <h1></h1>
-        <div className="image-container">
-          <img src={LilypadLogo} alt="Logo" className="logo-image" />
-        </div>
-      </div>
 
-      <div className="search-bar-container">
-        <input type="text" placeholder="Where do you want to go?" className="search-input" />
-        <button className="search-button">Search</button>
-      </div>
-      
-      <div className="title-container">
-        <h3>Explore the World with Us</h3>
-      </div>
+  return (
+    <div>
+      <TitleBanner />
+      <SearchBar />
     </div>
   );
 };
