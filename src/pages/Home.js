b3c7b7d2f4a298928b3793f8dda73b0c8f6241a1
Siham Dahir <98513419+sihamad@users.noreply.github.com>
2023-05-22 04:24:46 +0100

    Uploaded and imported logo to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,11 @@
 import TitleBanner from "../components/TitleBanner";
 import SearchBar from "../components/SearchBar";
+import React from 'react';
+import './Home.css'; 
+
 
 const Home = () => {
+
   return (
     <>
       <TitleBanner />
