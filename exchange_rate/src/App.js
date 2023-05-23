b7d432ab78b16f76321b9a802c28044c5b38a2fe
KayanatSuleman <kayanat.suleman@yahoo.co.uk>
2023-05-23 15:00:40 +0100

    Exchange rate api updated to a new one called: Api Layer, to allow for the base rate to be GBP for all exchange rate calculations.

--- exchange_rate/src/App.js
+++ exchange_rate/src/App.js
@@ -1,52 +1,72 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import './App.css';
 
-function App() {
-  const [countryTag, setCountryTag] = useState('');
-  const [exchangeRate, setExchangeRate] = useState('');
+const App = () => {
+  const [currencyCode, setCurrencyCode] = useState('');
+  const [exchangeRate, setExchangeRate] = useState(null);
+  const [errorMessage, setErrorMessage] = useState('');
 
-  const fetchExchangeRate = async () => {
-    try {
-      const response = await axios.get(
-        `https://openexchangerates.org/api/latest.json?app_id=55e6bd24eae746fdb830f16fe3612f87&symbols=${countryTag}`
-      );
-      const rate = response.data.rates[countryTag];
-      setExchangeRate(rate);
-    } catch (error) {
-      console.error(error);
+  const handleCurrencyChange = (event) => {
+    const value = event.target.value.toUpperCase();
+    setCurrencyCode(value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    if (currencyCode) {
+      const apiKey = 'lYgzjJUuEyZmoq2kMBRaecDpR68UQatT';
+      const url = `https://api.apilayer.com/exchangerates_data/convert?from=GBP&to=${currencyCode}&amount=1`;
+
+      axios
+        .get(url, {
+          headers: {
+            apikey: apiKey,
+          },
+        })
+        .then((response) => {
+          const { success, info, result } = response.data;
+
+          if (success) {
+            setExchangeRate(result);
+            setErrorMessage('');
+          } else {
+            setExchangeRate(null);
+            setErrorMessage(`Error: ${info}`);
+          }
+        })
+        .catch((error) => {
+          console.error('Error fetching exchange rate:', error);
+          setExchangeRate(null);
+          setErrorMessage('Error: Unable to fetch exchange rate');
+        });
     }
   };
 
-  const handleInputChange = (e) => {
-    setCountryTag(e.target.value.toUpperCase());
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    fetchExchangeRate();
-  };
-
   return (
-    <div className="card">
-      <h1>Exchange Rate Checker</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Country Tag:
+    <div className="app">
+      <h1>Exchange Rate App</h1>
+      <form className="exchange-form" onSubmit={handleFormSubmit}>
+        <div className="input-group">
+          <label htmlFor="currency-input">Currency Code:</label>
           <input
+            id="currency-input"
             type="text"
-            value={countryTag}
-            onChange={handleInputChange}
+            value={currencyCode}
+            onChange={handleCurrencyChange}
           />
-        </label>
+        </div>
         <button type="submit">Get Exchange Rate</button>
       </form>
-      {exchangeRate && (
-        <p>
-          Exchange Rate for {countryTag}: {exchangeRate}
+      {exchangeRate !== null && (
+        <p className="exchange-rate">
+          1 GBP = {exchangeRate.toFixed(6)} {currencyCode}
         </p>
       )}
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
     </div>
   );
-}
+};
 
 export default App;
