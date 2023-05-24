b68b4457d7c56f979dfbc545b2bd4e8457bb85c3
Karen <karenyaa@live.co.uk>
2023-05-24 08:14:00 +0100

    added state hook to add weather icons & removed unnecessary code

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,42 +3,50 @@ import WeatherIcon from "../images/weather_placeholder.png";
 import { useEffect, useState } from "react";
 
 function Weather(props) {
-    const {latitude, longitude} = props;
-    let city1 = "warsaw";
-    let lat = latitude;
-    let long = longitude;
-    const [temperature, setTemperature] = useState('');
-    const [humidity, setHumidity] = useState('');
-    //const [icon, setIcon] = useState('');
+  const { latitude, longitude } = props;
+  let city1 = "warsaw";
+  let lat = latitude;
+  let long = longitude;
+  const [temperature, setTemperature] = useState("");
+  const [humidity, setHumidity] = useState("");
+  const [icon, setIcon] = useState("");
 
-    function fromKelvinToCelsius(kelvin) {
-        return Math.round((kelvin - 273.15) * 10) / 10;
+  function fromKelvinToCelsius(kelvin) {
+    return Math.round((kelvin - 273.15) * 10) / 10;
+  }
+
+  useEffect(() => {
+    async function getWeatherData() {
+      let apiKey = "c38077db5d2d3cc7511c35c5146ebdb4";
+      let queryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude={part}&appid=${apiKey}`;
+
+      try {
+        const response = await fetch(queryURL, {
+          method: "GET",
+        });
+        console.log(city1, lat, long);
+        let data = await response.json();
+        console.log(data);
+      
+        setTemperature(fromKelvinToCelsius(data.current.temp) + "°C");
+        setHumidity(data.current.humidity + "%");
+        setIcon(data.current.weather[0].icon)
+      } catch (error) {
+        alert("There was an error fetching the data", error);
       }
+    }
 
-    useEffect(() => {
-        async function getWeatherData() {
-            let apiKey = "c38077db5d2d3cc7511c35c5146ebdb4"
-            let queryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude={part}&appid=${apiKey}`;
-    
-            const response = await fetch(queryURL, {
-                method: 'GET'
-            });
-            console.log(city1, lat,long)
-            let data = await response.json();
-            console.log(data)
-            setTemperature(fromKelvinToCelsius(data.current.temp) + "°C")
-            setHumidity(data.current.humidity + "%")
-            }
-            
-        getWeatherData();
-    });
+    getWeatherData();
+  });
 
   return (
     <div className="weather-component">
-      <h6 id="city"><b>{city1} </b></h6>
+      <h6 id="city">
+        <b>{city1} </b>
+      </h6>
       <img
         className="weather-icon"
-        src={WeatherIcon}
+        src={`http://openweathermap.org/img/w/${icon}.png`}
         alt="placeholder weather icon"
       />
       <div className="weather-info">
@@ -50,3 +58,4 @@ function Weather(props) {
 }
 
 export default Weather;
+
