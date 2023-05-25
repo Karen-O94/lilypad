3b98ae9b33c0055d3eaad2c4e41bd77b423f985d
Siham Dahir <98513419+sihamad@users.noreply.github.com>
2023-05-25 06:03:17 +0100

    Modified API call so it returns images per article

--- src/Components/TopNews.js
+++ src/Components/TopNews.js
@@ -3,22 +3,31 @@ const destinationInput = document.querySelector('#destination-input');
 const searchButton = document.querySelector('#search-button');
 const headlinesList = document.querySelector('#headlines');
 
-apikey = '62bcea3d0ec6311465039f3e2a53dda2';
+const apikey = '62bcea3d0ec6311465039f3e2a53dda2';
 
 form.addEventListener('submit', function(event) {
   event.preventDefault(); // this prevents the form from submitting and refreshing the page
-  
+
   const destination = destinationInput.value;
-  const url = `https://gnews.io/api/v4/top-headlines?q=${destination}&max=5&token=${apikey}`;
+  const url = `https://gnews.io/api/v4/top-headlines?q=${destination}&max=5&token=${apikey}&image=required`;
 
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      headlinesList.innerHTML = ''; // this will clear the existing list each time user enters new input
+      headlinesList.innerHTML = ''; // this will clear the existing list each time the user enters new input
 
       data.articles.forEach(article => {
         const li = document.createElement('li');
-        li.innerHTML = `<a href="${article.url}" target="_blank">${article.title}</a> - ${article.description}`;
+        const image = document.createElement('img');
+        image.src = article.image;
+        image.alt = 'Article Image';
+        const link = document.createElement('a');
+        link.href = article.url;
+        link.target = '_blank';
+        link.textContent = article.title;
+        li.appendChild(image);
+        li.appendChild(link);
+        li.innerHTML += ` - ${article.description}`;
         headlinesList.appendChild(li);
       });
     })
