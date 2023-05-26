0e4572cf8a2b60811df855d0ac3cb5a5f841ed70
Siham Dahir <98513419+sihamad@users.noreply.github.com>
2023-05-26 21:42:55 +0100

    Refactored API request into react

--- src/Components/TopNews.js
+++ src/Components/TopNews.js
@@ -1,14 +1,15 @@
 import { useState } from "react";
 
-function NewsHeadlines() {
-  const [headlines, setHeadlines] = useState([]);
+function NewsHeadlines() {   // creates function to fetch news headlines from API
+  const [headlines, setHeadlines] = useState([]);  //uses state to manage the fetched headlines
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
     const destination = event.target.elements.destination.value;
     const apiKey = 'f36795f9abc460d7b535bfe9ab2b93f5';
-    const queryURL = `https://gnews.io/api/v4/top-headlines?q=${destination}&max=5&image=required&token=${apiKey}`;
+    const queryURL = `https://gnews.io/api/v4/top-headlines?q=${destination}&max=5&lang=en&image=required&token=${apiKey}`;
 
+// fetches the news headlines from the API into json format and catches any errors
     try {
       const response = await fetch(queryURL, {
         method: "GET",
@@ -16,12 +17,14 @@ function NewsHeadlines() {
 
       const data = await response.json();
       setHeadlines(data.articles);
+    
     } 
     catch (error) {
       alert("There was an error fetching the data", error);
     }
   };
 
+  // this takes user input into the form 
   return (
     <div>
       <form onSubmit={handleFormSubmit}>
@@ -29,7 +32,6 @@ function NewsHeadlines() {
         <input type="text" name="destination" id="destination" />
         <button type="submit">Search</button>
       </form>
-
 
       <ul>
         {headlines.map((article, index) => (
